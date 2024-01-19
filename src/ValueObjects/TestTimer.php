021aa87e9af5268af3a14525326cf074492b4d77
Aggelos Bellos <aggelosbellos7@gmail.com>
2024-01-19 16:33:54 +0200

    Value objects for storing the time of each test

---
+++ src/ValueObjects/TestTimer.php
@@ -0,0 +1,68 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Bellangelo\TestSuiteArchitect\ValueObjects;
+
+use InvalidArgumentException;
+
+class TestTimer
+{
+    private string $name;
+    private float $startTime;
+    private float $endTime;
+
+    public function __construct(string $name, float $startTime, float $endTime = 0.0)
+    {
+        $this->setName($name);
+        $this->setStartTime($startTime);
+        $this->setEndTime($endTime);
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function getStartTime(): float
+    {
+        return $this->startTime;
+    }
+
+    public function getEndTime(): float
+    {
+        return $this->endTime;
+    }
+
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+
+    public function setEndTime(float $endTime): void
+    {
+        if ($endTime !== 0.0 && $endTime < $this->getStartTime()) {
+            throw new InvalidArgumentException('End time cannot be less than start time');
+        }
+
+        $this->endTime = $endTime;
+    }
+
+    public function setStartTime(float $startTime): void
+    {
+        if ($startTime < 0) {
+            throw new InvalidArgumentException('Start time cannot be negative');
+        }
+
+        $this->startTime = $startTime;
+    }
+
+    public function getElapsedTime(): float
+    {
+        if ($this->getEndTime() === 0.0) {
+            return 0;
+        }
+
+        return $this->getEndTime() - $this->getStartTime();
+    }
+}
---
+++ src/ValueObjects/TestTimerCollection.php
@@ -0,0 +1,47 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Bellangelo\TestSuiteArchitect\ValueObjects;
+
+use Iterator;
+
+class TestTimerCollection implements Iterator
+{
+    private array $testTimers = [];
+
+    public function add(TestTimer $testTimer): void
+    {
+        $this->testTimers[$testTimer->getName()] = $testTimer;
+    }
+
+    public function get(string $name): ?TestTimer
+    {
+        return $this->testTimers[$name] ?? null;
+    }
+
+    public function current(): TestTimer
+    {
+        return current($this->testTimers);
+    }
+
+    public function next(): TestTimer
+    {
+        return next($this->testTimers);
+    }
+
+    public function key(): string
+    {
+        return key($this->testTimers);
+    }
+
+    public function valid(): bool
+    {
+        return isset($this->testTimers[$this->key()]);
+    }
+
+    public function rewind(): void
+    {
+        reset($this->testTimers);
+    }
+}
