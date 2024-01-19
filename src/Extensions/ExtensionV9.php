b581f08437b9ffd8bb1862b778b0f1742f4aa4bb
Aggelos Bellos <aggelosbellos7@gmail.com>
2024-01-19 18:04:17 +0200

    Remove unused imports

--- src/Extensions/ExtensionV9.php
+++ src/Extensions/ExtensionV9.php
@@ -5,14 +5,9 @@ declare(strict_types=1);
 namespace Bellangelo\TestSuiteArchitect\Extensions;
 
 use Bellangelo\TestSuiteArchitect\TimeReporting;
-use PHPUnit\Framework\AssertionFailedError;
 use PHPUnit\Framework\Test;
 use PHPUnit\Framework\TestListener;
 use PHPUnit\Framework\TestListenerDefaultImplementation;
-use PHPUnit\Framework\TestSuite;
-use PHPUnit\Framework\Warning;
-use PHPUnit\Runner\BeforeTestHook;
-use Throwable;
 
 class ExtensionV9 extends TimeReporting implements TestListener
 {
