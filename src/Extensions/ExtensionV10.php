ae2fc6ae091c88e833cbdb3920094f7ee2456acc
Aggelos Bellos <aggelosbellos7@gmail.com>
2024-01-19 19:36:15 +0200

    Working at 2 versions at the same time is too much when the project is not final

--- src/Extensions/ExtensionV10.php
+++ src/Extensions/ExtensionV10.php
@@ -4,25 +4,12 @@ declare(strict_types=1);
 
 namespace Bellangelo\TestSuiteArchitect\Extensions;
 
-use Bellangelo\TestSuiteArchitect\TimeReporting;
-use PHPUnit\Runner\AfterLastTestHook;
-use PHPUnit\Runner\AfterTestHook;
-use PHPUnit\Runner\BeforeTestHook;
+use Exception;
 
-class ExtensionV10 extends TimeReporting implements BeforeTestHook, AfterTestHook, AfterLastTestHook
+class ExtensionV10 extends Extension
 {
-    public function executeBeforeTest(string $test): void
+    public function __construct()
     {
-        $this->storeStartTime($test);
-    }
-
-    public function executeAfterTest(string $test, float $time): void
-    {
-        $this->storeEndTime($test, $time);
-    }
-
-    public function executeAfterLastTest(): void
-    {
-        $this->storeReport();
+        throw new Exception('Not implemented yet');
     }
 }
