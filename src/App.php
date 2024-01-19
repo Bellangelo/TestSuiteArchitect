8b19586c21039da0e61cab365eea04a3962d7e5f
Aggelos Bellos <aggelosbellos7@gmail.com>
2024-01-20 00:01:10 +0200

    Refactor code to use a controller based approach

--- src/App.php
+++ src/App.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Bellangelo\TestSuiteArchitect;
 
-use Bellangelo\TestSuiteArchitect\Storage\TimeReportingHandler;
 use Minicli\App as MinicliApp;
 use Minicli\Command\CommandCall;
 
@@ -14,11 +13,7 @@ class App
 
     public function __construct()
     {
-        $this->setApp(
-            new MinicliApp()
-        );
-
-        $this->registerCommands();
+        $this->initApp();
     }
 
     public function runCommand(CommandCall $input): void
@@ -36,30 +31,15 @@ class App
         return $this->app;
     }
 
-    private function registerCommands(): void
+    private function initApp(): void
     {
-        $this->getApp()->registerCommand('partition', function (CommandCall $input) {
-            if ($input->getParam('number')) {
-                self::createTestSuites((int) $input->getParam('number'));
-            } else {
-                echo 'Please provide a number of partitions.' . PHP_EOL;
-            }
-        });
-    }
+        $app = new MinicliApp(
+            [
+                'app_path' => __DIR__ . '/Commands/',
+            ],
+            './testsuitearchitect'
+        );
 
-    public static function createTestSuites(int $numberOfPartitions): void
-    {
-        if ($numberOfPartitions < 2) {
-            echo 'Please provide a number of partitions greater than 1.' . PHP_EOL;
-            return;
-        }
-
-        $data = (new TimeReportingHandler())->readReport();
-
-        $partitions = (new TimeBasedPartitions($data))->createPartitions($numberOfPartitions);
-
-        foreach ($partitions as $partition) {
-            echo implode(',', $partition) . PHP_EOL;
-        }
+        $this->setApp($app);
     }
 }
---
+++ src/Commands/Partition/DefaultController.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Bellangelo\TestSuiteArchitect\Commands\Partition;
+
+use Bellangelo\TestSuiteArchitect\Storage\TimeReportingHandler;
+use Bellangelo\TestSuiteArchitect\TimeBasedPartitions;
+use Minicli\Command\CommandController;
+use Throwable;
+
+class DefaultController extends CommandController
+{
+    public function handle(): void
+    {
+        $numberOfPartitions = (int) $this->getParam('number');
+
+        if ($numberOfPartitions < 2) {
+            echo 'Please provide a number of partitions greater than 1.' . PHP_EOL;
+            return;
+        }
+
+        try {
+            $data = (new TimeReportingHandler())->readReport();
+
+            $partitions = (new TimeBasedPartitions($data))->createPartitions($numberOfPartitions);
+
+            foreach ($partitions as $partition) {
+                echo implode(',', $partition) . PHP_EOL;
+            }
+        } catch (Throwable $e) {
+            echo $e->getMessage() . PHP_EOL;
+            return;
+        }
+    }
+
+    public function required(): array
+    {
+        return [
+            'number'
+        ];
+    }
+}
