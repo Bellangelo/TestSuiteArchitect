b3bf65a0a92d68ea154befde15dc142d77e17163
Aggelos Bellos <aggelosbellos7@gmail.com>
2024-01-22 00:40:34 +0200

    Class to evenly split items in x parts

---
+++ src/Partitions/LoadBalancingPartitions.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Bellangelo\TestSuiteArchitect\Partitions;
+
+use Bellangelo\TestSuiteArchitect\ValueObjects\TestTimerCollection;
+
+class LoadBalancingPartitions extends Partitions
+{
+    public function createPartitions(int $numberOfPartitions): array
+    {
+        $array = $this->getData()->toArray();
+        $totalElements = count($array);
+        $result = [];
+
+        $basePartSize = floor($totalElements / $numberOfPartitions);
+        // Extra items to distribute
+        $extraElements = $totalElements % $numberOfPartitions;
+
+        $startIndex = 0;
+
+        for ($i = 0; $i < $numberOfPartitions; $i++) {
+            // Calculate the size of the current part
+            $partSize = $basePartSize + ($i < $extraElements ? 1 : 0);
+
+            $result[] = array_slice($array, $startIndex, $partSize);
+
+            $startIndex += $partSize;
+        }
+
+        return $result;
+    }
+}
