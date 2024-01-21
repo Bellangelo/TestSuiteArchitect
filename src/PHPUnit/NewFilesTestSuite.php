c3c47224318af72e67573b1399e1582631468471
Aggelos Bellos <aggelosbellos7@gmail.com>
2024-01-22 01:51:48 +0200

    Method for adding new files for current partition only

--- src/PHPUnit/NewFilesTestSuite.php
+++ src/PHPUnit/NewFilesTestSuite.php
@@ -4,24 +4,47 @@ declare(strict_types=1);
 
 namespace Bellangelo\TestSuiteArchitect\PHPUnit;
 
+use Bellangelo\TestSuiteArchitect\Partitions\LoadBalancingPartitions;
 use Bellangelo\TestSuiteArchitect\Storage\StorageHandler;
 use Bellangelo\TestSuiteArchitect\Storage\TimeReportingHandler;
+use Bellangelo\TestSuiteArchitect\ValueObjects\TestTimer;
+use Bellangelo\TestSuiteArchitect\ValueObjects\TestTimerCollection;
 use http\Exception\RuntimeException;
 use PHPUnit\Framework\TestSuite;
 use RecursiveDirectoryIterator;
 use RecursiveIteratorIterator;
 
-class NewFilesTestSuite extends TestSuite
+abstract class NewFilesTestSuite extends TestSuite
 {
-    protected static function findNewFiles(): array
+    protected static function addNewTestsForCurrentPartition(
+        TestSuite $suite,
+        int $numberOfPartitions,
+        int $index
+    ): void {
+        $newTests = self::getNewTestFiles();
+        $partitions = (new LoadBalancingPartitions($newTests))->createPartitions($numberOfPartitions);
+
+        if (isset($partitions[$index])) {
+            foreach ($partitions[$index] as $test) {
+                $suite->addTestFile($test->getName());
+            }
+        }
+    }
+
+    protected static function getNewTestFiles(): TestTimerCollection
     {
         $filesInReport = (new TimeReportingHandler())->readReport();
         $testFiles = self::getTestFiles();
-        $newFiles = [];
+        $newFiles = new TestTimerCollection();
 
         foreach ($testFiles as $file) {
             if (!$filesInReport->get($file)) {
-                $newFiles[] = $file;
+                $newFiles->add(
+                    new TestTimer(
+                        $file->getPathname(),
+                        0
+                    )
+                );
             }
         }
 
